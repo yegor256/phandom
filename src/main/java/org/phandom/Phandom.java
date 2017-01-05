3c4f7151421753270d580de102c1b5619e396096
Yegor Bugayenko <yegor256@gmail.com>
2017-01-05 09:36:45 +0200

    Up to 2017, Happy New Year!

--- src/main/java/org/phandom/Phandom.java
+++ src/main/java/org/phandom/Phandom.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2014, phandom.org
+ * Copyright (c) 2013-2017, phandom.org
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
